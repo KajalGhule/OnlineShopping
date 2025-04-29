33bf8ac0ee44183b66dc5f7aea4c9222cc1f3328
KajalGhule <kajalghule111@gmail.com>
2025-04-29 10:12:00 +0530

    Demo on delegate

---
+++ DelegeteAndEvent/DelegeteAndEvent/Delegates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegeteAndEvent
+{
+    public class Delegates
+    {
+        public delegate void Handler();
+        public static void PayIncomeTax()
+        {
+            Console.WriteLine("PayIncomeTax");
+        }
+        public static void PayPropertyTax()
+        {
+            Console.WriteLine(" PayPropertyTax");
+        }
+        public static void PayServiceTax()
+        {
+            Console.WriteLine("PayServiceTax ");
+        }
+    }
+}
---
+++ DelegeteAndEvent/DelegeteAndEvent/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using DelegeteAndEvent;
+
+using static DelegeteAndEvent.Delegates;
+
+Console.WriteLine("Hello, World!");
+
+//early binding
+//PayIncomeTax();
+
+//Late binding Unicast delegate
+Handler operation1 = null;
+operation1 = new Handler(PayIncomeTax); // registering name of function to be invoke
+//operation1();
+
+Handler operation2 = null;
+operation2 = new Handler(PayPropertyTax);
+//operation2();
+
+Handler operation3 = null;
+operation3 = new Handler(PayServiceTax);
+//operation3();
+
+
+Handler masterOperationManager = null; // Multicast
+masterOperationManager = operation1;
+masterOperationManager += operation2;
+masterOperationManager += operation3;
+masterOperationManager(); // One invokation multicast delegate
